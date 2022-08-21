804c79fa50d179bcfa47a409e3fbe3ea182330ca
katredford <katharine.redford@gmail.com>
2022-08-20 21:58:34 -0500

    add form component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
-
+import Form from './components/Form'
 import Header from './components/Header'
 
 function App() {
   return (
     <div >
       <Header />
-     
+     <Form />
     </div>
   );
 }
---
+++ src/components/Form/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Form() {
+  return (
+      <div>
+    <main>
+            <form>
+                <input type="text" />
+                <input type="text" />
+                <button>Create Meme</button>
+            </form>
+        </main>
+  </div>
+  )
+}
